71bd76fcf3f94e1e7e70ff5c8b4eceba47f8ae14
Rafi Hanif <rafizmujahid86@gmail.com>
2019-04-29 11:56:00 +0700

    first commit

---
+++ app/src/main/java/rafihanif/libraryretrofit/MainActivity.java
@@ -0,0 +1,57 @@
+package rafihanif.libraryretrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.List;
+
+import rafihanif.libraryretrofit.adapter.MoviesAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit = null;
+    private RecyclerView recyclerView = null;
+    // Masukkan API key themoviedb.org API KEY di bawah ini
+    private final static String API_KEY = "540b210f4e24d7ed3e5d8023867a0f79";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        connectAndGetApiData();
+    }
+
+    public void connectAndGetApiData(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<MovieResponse> call = movieApiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+                Log.d(TAG, "Jumlah film: " + movies.size());
+            }
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
+    }
+}
